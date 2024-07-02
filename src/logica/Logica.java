72964726fb6a5efc1ba5f651f8e72b7a3f596e2e
David Valdez Gramajo <davidpivito@gmail.com>
2024-07-02 17:32:57 -0300

    uploading files

---
+++ src/logica/Logica.java
@@ -0,0 +1,13 @@
+package logica;
+import igu.Pantalla;
+
+public class Logica {
+    public static void main(String[] args){
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        panta.setTitle("Mi calculadora");
+        
+        
+    }
+}
